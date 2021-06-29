268f01e7de1a0620cd61feddd9887d261f3885fd
Dennis <lieudennis7@gmail.com>
2021-06-28 21:12:44 -0600

    Fixed time values to always display 2 digits

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,15 +4,15 @@ const Timer = () => {
   const defaultTime = { hr: 0, min: 0, sec: 0, ms: 0 };
 
   const [time, setTime] = useState(defaultTime);
-  const [timerOn, setTimerOn] = useState(false);
+  const [isTimerOn, setisTimerOn] = useState(false);
 
   return (
     <section>
       <p>
-        <span>{time.hr}:</span>
-        <span>{time.min}:</span>
-        <span>{time.sec}:</span>
-        <span>{time.ms}:</span>
+        {time.hr > 10 ? <span>{time.hr}:</span> : <span>0{time.hr}:</span>}
+        {time.min > 10 ? <span>{time.min}:</span> : <span>0{time.min}:</span>}
+        {time.sec > 10 ? <span>{time.sec}:</span> : <span>0{time.sec}:</span>}
+        {time.ms > 10 ? <span>{time.ms}:</span> : <span>0{time.ms}</span>}
       </p>
     </section>
   );
