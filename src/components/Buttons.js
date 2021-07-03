3179aaeefe8e16d6b77d6164f45ddcaf30fbbcbe
Dennis <lieudennis7@gmail.com>
2021-07-03 13:39:27 -0600

    Changed buttons to display stop/start etc

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,9 +3,8 @@ import React from "react";
 const Buttons = ({ isTimerOn, startTimer, stopTimer }) => {
   return (
     <div>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-      <button>Lap</button>
+      {!isTimerOn ? <button onClick={startTimer}>Start</button> : <button onClick={stopTimer}>Stop</button> }
+      {!isTimerOn ? <button>Reset</button> : <button>Lap</button>}
     </div>
   );
 };
